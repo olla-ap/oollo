bb148898a58eb8cdf3633839bc71f0d595f8cc5a
a wang <1229511960@qq.com>
2026-09-10 22:52:25 +0800

    task1

---
+++ src/GreetingService.java
@@ -0,0 +1,9 @@
+public class GreetingService {
+    //提供带有字符串参数的public void方法
+    public void sayHello(String name){
+        //在方法内部打印
+        System.out.println("Hello!");
+        //(可选择）如果想要更完整，可以打印传入的名字
+        //System.out.println("Hello," + name + "!");
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        //1.实例化greetingservice类
+GreetingService service = new
+        GreetingService();
+//2.调用sayhello方法，上传一个名字参数
+
+service.sayHello("World");
+    }
+    }
